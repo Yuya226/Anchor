cc0d6842ed9b34b51e6356960473de58cf53d518
github Yuya226 <yuya.k.0226@gmail.com>
2026-05-17 01:01:15 +0900

    Initial Commit

---
+++ Anchor/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Anchor
+//
+//  Created by Yuya Kubo on 2026/05/17.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AnchorTests/AnchorTests.swift
@@ -0,0 +1,19 @@
+//
+//  AnchorTests.swift
+//  AnchorTests
+//
+//  Created by Yuya Kubo on 2026/05/17.
+//
+
+import Testing
+@testable import Anchor
+
+struct AnchorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
